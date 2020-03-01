aabd2d86a330b403459b22ab242f0b309cef748d
Joel Maykol Castro Espinosa <jmc.espinosa@yavirac.edu.ec>
2020-03-01 18:19:40 -0500

    validaciones

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
@@ -15,7 +15,9 @@ export class ClienteComponent implements OnInit {
   tabla: string;
   clienteForm: FormGroup;
   FormBuilder: any;
-  constructor(private http: HttpClient, private router: Router) { }
+  FormCliente: FormGroup;
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) {
+   }
 
   ngOnInit() {
 
@@ -27,18 +29,39 @@ export class ClienteComponent implements OnInit {
       email: '',
     };
     this.tabla = 'clientes';
+
+    this.formularioValidacion();
+  }
+  formularioValidacion() {
+    return this.clienteForm = new FormGroup({
+      // tslint:disable-next-line:max-line-length
+      identificacion: new FormControl ('', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^([0-9])*$')]),
+      nombre: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      hora: new FormControl ('', [Validators.required]) ,
+      // tslint:disable-next-line:max-line-length
+      email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+    });
   }
   Guardar = () => {
-    this.data = {
-      tabla: this.tabla,
-      datos: this.clientes
-    };
-    this.tabla = 'clientes';
-    this.http.post(environment.ApI_URL + 'post', this.data).subscribe(resultado => {
-      console.log(resultado);
-      alert ('registro Exitoso');
-    });
-    this.router.navigate(['asistencias']);
-  }
-
+     if (this.clienteForm.valid) {
+      this.data = {
+        tabla: this.tabla,
+        datos: this.clientes
+      };
+      this.tabla = 'clientes';
+      this.http.post(environment.ApI_URL + 'post', this.data).subscribe(resultado => {
+        console.log(resultado);
+        alert ('registro Exitoso');
+      });
+      this.router.navigate(['asistencias']);
+     } else {
+       alert('Algunos Datos son incorrectos');
+     }
+    }
+    get identificacion() {return this.clienteForm.get('identificacion'); }
+    get nombre() {return this.clienteForm.get('nombre'); }
+    get apellido() {return this.clienteForm.get('apellido'); }
+    get hora() {return this.clienteForm.get('hora'); }
+    get email() {return this.clienteForm.get('email'); }
 }
--- src/app/profesores/profesores.component.ts
+++ src/app/profesores/profesores.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormControlName } from '@angular/forms';
 import { Profesor } from 'src/app/modulos/profesores';
 @Component({
   selector: 'app-profesores',
@@ -15,6 +15,7 @@ export class ProfesoresComponent implements OnInit {
   tabla: string;
   profesoresForm: FormGroup;
   FormBuilder: any;
+  FormProfesores: FormGroup;
   constructor(private http: HttpClient, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -26,28 +27,39 @@ export class ProfesoresComponent implements OnInit {
       email: '',
     };
     this.tabla = 'profesores';
-
+    this.formularioProfesores();
+  }
+  formularioProfesores() {
+    return this.profesoresForm = new FormGroup({
+      // this.FormProfesores = new FormGroup({
+      // tslint:disable-next-line:max-line-length
+      identificacion: new FormControl ('', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^([0-9])*$')]),
+      nombre: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      apellido: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      // tslint:disable-next-line:max-line-length
+      email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+      titulosProfesionaels: new FormControl('', [Validators.required, Validators.minLength(4)])
+    // });
+    });
   }
   Guardar = () => {
-    this.data = {
-      tabla: this.tabla,
-      datos: this.profesores
-    };
-    this.http.post(environment.ApI_URL + 'post', this.data).subscribe(resultado => {
-      console.log(resultado);
-      alert ('registro Exitoso');
-      this.router.navigate(['']);
-    });
+    if (this.profesoresForm.valid) {
+      this.data = {
+        tabla: this.tabla,
+        datos: this.profesores
+      };
+      this.http.post(environment.ApI_URL + 'post', this.data).subscribe(resultado => {
+        console.log(resultado);
+        alert ('registro Exitoso');
+        this.router.navigate(['']);
+      });
+    } else {
+      alert ('No valido');
+    }
   }
-
-  formularioProfesores() {
-    this.profesoresForm = this.formBuilder.group({
-      identificacion: ['', [Validators.required]],
-      nombre: ['', [Validators.required]],
-      apellido: ['', [Validators.required]],
-      email: ['', [Validators.required]],
-      titulosProfesionaels: ['', [Validators.required]]
-    });
-  }
-
+  get identificacion() {return this.profesoresForm.get('identificacion'); }
+  get nombre() {return this.profesoresForm.get('nombre'); }
+  get apellido() {return this.profesoresForm.get('apellido'); }
+  get email() {return this.profesoresForm.get('email'); }
+  get titulosProfesionaels() {return this.profesoresForm.get('titulosProfesionaels'); }
 }
